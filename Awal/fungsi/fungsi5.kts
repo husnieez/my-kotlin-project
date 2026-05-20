a18d9911c864172ec63f3fd1a137aa568b32486a
husnieez <husnimuszdalifah.kuliah@gmail.com>
2026-05-20 12:03:18 +0700

    Add Kotlin script examples for function parameters, named arguments, and default values

---
+++ Awal/fungsi/fungsi5.kts
@@ -0,0 +1,12 @@
+#!/usr/bin/env kotlin
+
+fun main() {
+    println(birthdayGreeting("Rover", 5))
+    println(birthdayGreeting(age = 2, name = "Rex"))
+
+}
+fun birthdayGreeting(name: String, age: Int): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now $age years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
