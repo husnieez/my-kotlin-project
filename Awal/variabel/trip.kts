773dbc6d870c69b4840ed54889e47c77a9bcfe77
husnieez <husnimuszdalifah.kuliah@gmail.com>
2026-05-21 15:18:11 +0700

    Add Kotlin script examples for function parameters, named arguments, and default values

---
+++ Awal/variabel/trip.kts
@@ -0,0 +1,9 @@
+#!/usr/bin/env kotlin
+
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLenght: Double = trip1 + trip2 + trip3
+    println("$totalTripLenght miles left to destination")
+}
